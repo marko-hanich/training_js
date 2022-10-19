7d266d4fb840b77b9d34643a4748fb4d816c9522
marko-hanich <ganichmark2017@gmail.com>
2022-10-19 19:50:40 +0200

    Update training.js

--- js/training.js
+++ js/training.js
@@ -185,3 +185,25 @@
 
 // });
 
+
+//!СОЗДАНИЕ КАЛЬКУЛЯТОРА ВЫЧИСЛЕНИЯ КВАДРАТНОГО УРАВННЕНИЯ
+
+let a = 1, b = -2, c = -3;
+
+let D = b * b - 4 * a * c;
+
+if (D < 0) {
+    console.log("Корней не существует");
+} else if (D == 0) {
+    let x = -b / 2 * a;
+    console.log("x="+x)
+} else {
+    let x1 = (-b + Math.sqrt(D)) / 2 * a;
+    let x2 = ((-b - Math.sqrt(D)) / 2) * a;
+    console.log("x1=" + x1 + "; x2=" + x2);
+}
+
+console.log("Дискриминант = " + D);
+
+
+
